b41bde835a076f9b6027585bedef313b29c517eb
d3m1rt45 <d3m1rt45@outlook.com>
2019-12-06 19:52:29 +0000

    CDNS, jQuery, Rest
    Some CDNS are set in place for convenience at the front end.
    
    A block of jQuery code retreives the related api as we type in the textbox that has been created.

---
+++ GithubFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GithubFinder.MvcApplication" Language="C#" %>
---
+++ GithubFinder/Scripts/main.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#searchUser").on("keyup", function (e) {
+        let username = e.target.value;
+
+        //Make request to Github
+        $.ajax({
+            url: "https://api.github.com/users/" + username,
+            data: {
+                client_id: "70a95d802b290379ba34",
+                client_secret: "5306be33d0df19dff784f80545c4d82f4af07b0e"
+            }
+        }).done(function (user) {
+            $("#profile").html(`
+                ${user.name}
+            `);
+        });
+    });
+});
